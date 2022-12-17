b00fdcc28dda1491dd5f41b8982dad7692a053e6
bruks <ats@sl.ru>
2022-12-17 16:33:54 +0300

    locales/ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,378 @@
+export const ru = {
+    // topnav
+    Search: 'Поиск',
+    FullScreen: 'На весь экран',
+    Refresh: 'Обновить',
+    selected: 'Выбрано',
+  
+    LogIn: 'Войти',
+    LogOut: 'Выйти',
+    SignUp: 'Зарегистрироваться',
+    SignIn: 'SignIn',
+  
+    // sidenav
+    Alerts: 'Алерты',
+    Heartbeats: 'Пульс',
+    Users: 'Пользователи',
+    Groups: 'Группы',
+    Customers: 'Клиенты',
+    Blackouts: 'Скрытые',
+    Permissions: 'Права',
+    APIKeys: 'Ключи API',
+    Searches: 'Searches',
+    Labels: 'Метки',
+    Reports: 'Отчеты',
+    More: 'Еще',
+    Settings: 'Настройки',
+    Help: 'Помощь',
+    About: 'О системе',
+  
+    // more
+    ChooseDisplayDensity: 'ВыбратьРежимОтображения',
+    DisplayDensity: 'РежимОтображения',
+    Comfortable: 'Комфортный',
+    Compact: 'Компактный',
+    Panel: 'Панель',
+    Hide: 'Скрыть',
+    Show: 'Показать',
+    DownloadAsCsv: 'Скачать csv',
+  
+    // Groups store
+    UserAddedGroup: 'Пользователь добавлен в группу.',
+    UserRemovedGroup: 'Пользователь удален из группы.',
+  
+    // Preferences store
+    SettingsSaved: 'Paramétrage sauvegardé.',
+    SettingsReset: 'Paramètres réinitialisés aux valeurs par défaut.',
+    SettingsError: 'Impossible de récupérer les préférences de l\'utilisateur.',
+  
+    // Users store
+    UserStatusSaved: 'Statut utilisateur enregistré.',
+    EmailSaved: 'Email vérifié enregistré.',
+  
+    // statuses
+    Open: 'Новый',
+    Assign: 'Assign',
+    Ack: 'Просмотрен',
+    Shelved: 'Отложен',
+    Blackout: 'Скрыт',
+    Closed: 'Закрыт',
+    Expired: 'Истек',
+    Unknown: 'Неизвестно',
+    NotValid: 'NotValid',
+  
+    // operator actions
+    Open: 'Open', //'Открыть'
+    Assign: 'Assign',
+    Ack: 'Ack', //'Просмотреть',
+    Unack: 'Unack', //'Пометить непросмотренным',
+    Shelve: 'Shelve', //'Отложить',
+    Unshelve: 'Вернуть',
+    Close: 'Close', //'Закрыть',
+    Watch: 'Watch', //'Следить',
+    Unwatch: 'Unwatch', //'Отменить отслеживание',
+    AddNote: 'Add note', //'Добавить коментарий',
+    Delete: 'Delete', //'Удалить',
+  
+    // Alert actions
+    TextIsRequired: 'Text is required',
+    TextMustBeLessThan: 'Text must be less than',
+    characters: 'characters',
+  
+    // Alert detail
+    AlertId: 'Id алерта',
+    AlertOrNoteId: 'Id алерта или комментария',
+    LastReceiveAlertId: 'ID последнего полученного алерта',
+    CreateTime: 'Дата создания',
+    ReceiveTime: 'Дата получения',
+    LastReceiveTime: 'Дата последнего полученного алерта',
+    Customer: 'Клиент',
+    Service: 'Служба',
+    Environment: 'Окружение',
+    Resource: 'Ресурс',
+    Event: 'Событие',
+    Correlate: 'Correlate',
+    Group: 'Группа',
+    Severity: 'Приоритет',
+    Status: 'Статус',
+    Value: 'Значение',
+    Text: 'Текст',
+    TrendIndication: 'TrendIndication',
+    Timeout: 'Timeout',
+    Type: 'Тип',
+    DuplicateCount: 'Счетчик дублей',
+    Repeat: 'Повтор',
+    Origin: 'Origin',
+    Tags: 'Тэги',
+    Attributes: 'Атрибуты',
+    History: 'История',
+    Data: 'Данные',
+  
+    Details: 'Инфо',
+    addedNoteOn: 'Комментарий добавлен',
+    by: 'пользователем',
+  
+    // Alert history
+    UpdateTime: 'UpdateTime',
+    Updated: 'Updated',
+    User: 'Пользователь',
+  
+    // Alert list
+    Loading: 'Loading',
+    NoDataAvailable: 'Данные отсутствуют',
+    ALL: 'ВСЕ',
+    Description: 'Описание',
+    Attribute: 'Атрибут',
+    TimeoutLeft: 'Осталось',
+    Dupl: 'Дуплоны',
+    PrevSeverity: 'Пред. Приоритет',
+    Duration: 'Длительность',
+    LastReceiveId: 'LastReceiveId',
+    LastNote: 'Последний комментарий',
+  
+    // Alert list filter
+    Filters: 'Фильтры',
+    FilterDescription: 'Описание фильтра',
+    AllEnvironments: 'AllEnvironments',
+    EnvironmentDescription: 'EnvironmentDescription',
+    AllSeverities: 'Все приоритеты',
+    SeverityDescription: 'Описание приоритетов',
+    AllStatuses: 'Все статусы',
+    StatusDescription: 'Описание статуса',
+    AllCustomers: 'Все клиенты',
+    CustomerDescription: 'Описание клиентов',
+    AllServices: 'Все службы',
+    ServiceDescription: 'Описание служб',
+    AllGroups: 'Все группы',
+    GroupDescription: 'Описание групп',
+  
+    Latest: 'Последние',
+    Hour: '1 час',
+    SixHours: '6 часов',
+    TwelveHours: '12 часов',
+    SelectRange: 'Выбрать период',
+  
+    DateTime: 'Дата/Время',
+    StartDate: 'Дата начала',
+    EndDate: 'Дата конца',
+    Time: 'Время',
+    Apply: 'Применить',
+    Reset: 'Сбросить',
+  
+    // API keys
+    APIKey: 'Ключ API',
+    Search: 'Поиск',
+    Customer: 'Клиент',
+    Scopes: 'Права',
+    Expires: 'Истекает',
+    Active: 'Активный',
+    Expired: 'Истек',
+    Slow: 'Slow',
+  
+    User: 'Пользователь',
+    LastUsed: 'Последнее использование',
+    Actions: 'Действия',
+  
+    Copy: 'Копировать',
+    Copied: 'Скопировано!',
+    NewApiKey: 'Новый ключ API',
+    EditApiKey: 'Редактировать ключ API',
+  
+    // Blackouts
+    ChooseService: 'Choisir un ou plusieurs service',
+    Start: 'Début',
+    End: 'Fin',
+    Created: 'Date de création',
+    Reason: 'Raison',
+    NewBlackout: 'Nouvel incident',
+    EditBlackout: 'Modifier incident',
+    Active: 'Actif',
+    Pending: 'En Attente',
+    Expired: 'Expiré',
+    WholeEnvironment: 'Whole Environment',
+    AllOrigin: 'All from Origin',
+  
+    // Customers
+    LookUp: 'Chercher',
+    LookUpDescription: 'Utiliser l\'identifiant, le rôle Keycloak, l\'organisation GitHub, le groupe GitLab ou le domaine de messagerie',
+    NewCustomer: 'Nouveau client',
+    EditCustomer: 'Modifier client',
+  
+    // Groups
+    AddRemoveUsers: 'Ajouter/Modifier utilisateurs',
+    Addusers: 'Ajouter utilisateurs',
+    UsersInGroup: 'Utilisateurs du groupe',
+    NumberUsers: 'Nombre d\'utilisateurs',
+    NewGroup: 'Nouveau groupe',
+    EditGroup: 'Modifier groupe',
+  
+    // Heartbeats
+    Latency: 'Latence',
+    Since: 'Depuis',
+  
+    // Manifest
+    API: 'API',
+    OpenGitHub: 'Ouvrir dans GitHub',
+    WebUI: 'Web UI',
+    Build: 'Build',
+    Date: 'Date',
+    GitRevision: 'Git Revision',
+    APIEndpoint: 'API Endpoint',
+  
+    // Perms
+    Role: 'Rôle',
+    Scope: 'Périmètre',
+    SystemRole: 'Rôle Système',
+    NewPermission: 'Nouvelle permission',
+    EditPermission: 'Modifier permission',
+  
+    // Preferences
+    ApplicationSettings: 'Paramètres d\'application',
+    DarkTheme: 'Thème sombre',
+    PlaySounds: 'Son notifications',
+    LanguageSettings: 'Paramétrage de la langue',
+    Languages: 'Langues',
+    English: 'Anglais',
+    French: 'Français',
+    German: 'Allemand',
+    Turkish: 'Turque',
+    Russian: 'Russian',
+    DateTimeSettings: 'Paramétrage date et heure',
+    LongDate: 'Format de date longue',
+    MediumDate: 'Format de date moyenne',
+    ShortTime: 'Format de temps courte',
+    DisplayMode: 'Mode d\'affichage',
+    UseLocal: 'Date et heure locale',
+    UseUTC: 'Utiliser le temps universel (UTC)',
+    AlertSettings: 'Paramétrage des alertes',
+    ShowAllowedEnvs: 'Toujours afficher les environnements autorisés',
+    ShowNotesIcon: 'Afficher l\'icône des notes',
+    ShowNotesHint: 'Afficher l\'icône à côté de l\'état de l\'alerte pour indiquer une note d\'opérateur',
+    Font: 'Texte',
+    FontSize: 'Taille de la police',
+    FontWeight: 'Niveaux de graisse',
+    PageRows: 'Lignes par page',
+    rows: 'lignes',
+    ValueWidth: 'Value Width',
+    DescriptionWidth: 'Description Text Width',
+    RefreshInterval: 'Intervalle de rafraîchissement',
+    seconds: 'secondes',
+    minutes: 'minutes',
+    AckTimeout: 'Durée de mise en affecter',
+    ShelveTimeout: 'Durée de mise en attente',
+    BlackoutSettings: 'Paramètres de blackout',
+    BlackoutStartNow: 'Commencer immédiatement la période d\'incident',
+    BlackoutPeriod: 'Durée de la période d\'incident',
+    hours: 'heures',
+  
+    // Profile
+    Profile: 'Profil',
+    UserID: 'User ID',
+    PrimaryUserID: 'Principal User ID',
+    Provider: 'Provider',
+    EmailVerified: 'Email verifié',
+    EmailNotVerified: 'Email non verifié',
+    Customers: 'Clients',
+    Organizations: 'Organisations',
+    Groups: 'Groupes',
+    Roles: 'Rôles',
+    Scopes: 'Périmètres',
+    others: 'autres',
+  
+    // Status
+    LastUpdate: 'Dernière mise à jour',
+    Uptime: 'Disponibilité',
+    Metric: 'Métrique',
+    Type: 'Type',
+    Name: 'Nom',
+    Value: 'Valeur',
+    AvgTime: 'Temps Moyen',
+    
+    // Users
+    Active: 'Actif',
+    Inactive: 'Inactif',
+    Login: 'Identifiant',
+    Email: 'Email',
+    Verified: 'Vérifié',
+    Comment: 'Commentaire',
+    VerifiedOrNot: 'Vérifié ?',
+    LastLogin: 'Dernière connexion',
+    NewUser: 'Nouvel utilisateur',
+    EditUser: 'Modifier utilisateur',
+    
+    // Reports
+    Top: 'Top',
+    Flapping: 'des signaux de présence',
+    Offenders: 'du nombres d\'alerte',
+    Standing: 'des alertes constantes',
+    TopFlappingDescription: 'Sources d\'alerte ayant généré le plus grand nombre de changements de gravité d\'alerte.',
+    TopOffendersDescription: 'Sources d’alerte fréquentes ayant généré le plus grand nombre d’alertes et de doublons d’alerte.',
+    TopStandingDescription: 'Sources d\'alertes obsolètes ayant des alertes actives pendant une durée prolongée.',
+    
+    Count: 'Compteur',
+    DuplCount: 'Doublons',
+    Services: 'Services',
+    Resources: 'Ressources',
+  
+    // User Confirm
+    Thanks: 'Merci !',
+    YouCanNowLogin1: 'Vous pouvez maintenant',
+    YouCanNowLogin2: 'login.',
+    EmailConfirmFailed: 'Désolé, un problème est survenu lors de la confirmation de votre email.',
+    TryAgain: 'Veuillez réessayer',
+  
+    // User Forgot
+    ResetLink: 'Entrez votre email et nous vous enverrons un lien de réinitialisation',
+    AlreadyHaveAccount: 'Vous avez déjà un compte?',
+    CheckEmail: 'Vérifiez votre email pour un lien de réinitialisation...',
+    ReturnSignIn: 'Retour à la connexion',
+    ResetEmailSent: 'Réinitialiser l\'email envoyé avec succès!',
+   
+    // User Login
+    LoginToContinue: 'Se connecter à Alerta pour continuer',
+    Username: 'Identifiant',
+    Password: 'Mot de passe',
+    CreateAccount: 'Créer un compte',
+    ForgotPassword: 'Mot de passe oublié?',
+    UnspecifiedProblem: 'Désolé, il y a eu un problème',
+    AuthWith: 'Authentification avec',
+    AuthInProgress: 'Authentification en cours...',
+    AuthNotPossible: 'Désolé, il n\'est pas possible de vous authentifier',
+    AuthProviderUnknown: 'Fournisseur d\'authentification inconnu',
+  
+    // User Logout
+    LoggedOut: 'Vous avez été déconnecté.',
+  
+    // User Reset
+    ChooseNewPassword: 'Choisissez un nouveau mot de passe',
+    ConfirmPassword: 'Confirmer mot de passe',
+    ResetPassword: 'Réinitialiser le mot de passe',
+  
+    // User Signup
+    CreateAlertaAccount: 'Créez votre compte Alerta',
+    SignUpNotAvailable: 'Désolé, l\'inscription n\'est pas disponible actuellement',
+    FullName: 'Nom complet',
+    Min6Char: '6 caractère minimum',
+    PasswordNotMatch: 'Le mot de passe entré ne correspond pas',
+  
+    // Labels
+    scope: 'périmètre',
+    role: 'rôle',
+    customer: 'client',
+    group: 'groupe',
+  
+    // General
+    ConfirmDelete: 'Êtes-vous sûr de bien vouloir supprimer cet élément?',
+    Error: 'Erreur',
+    Send: 'Envoyer',
+    Required: 'Requis',
+    Cancel: 'Annuler',
+    Save: 'Sauvegarder',
+    NoDisplay: 'Désolé, rien à afficher ici :(',
+    SearchNoResult1: 'Votre recherche pour ',
+    SearchNoResult2:  'n\'a trouvée aucun résultat.',
+    OK: 'OK',
+
+
+    // ENDS.
+  }
